c859e59af86971ab32d04f287d124df759803c61
wilson alexander <wilgm0kdev@gmail.com>
2024-08-03 22:39:45 -0500

    cambiando a modules

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express')
+// const express = require('express')
+import express from 'express'
 
 //crear app
 const app = express()
 
 //Routing
 app.get('/', function (req,res){
-    res.send('hola mundo en express')
+    res.send('Hola mundo en express')
 })
+app.get('/nosotros', function (req,res){
+    res.json({msg:"hola mundo"})
+})
+// app.get('/render', function (req,res){
+//     res.render()
+// })
 
 //definir un puerto y arrancar el proyecto
 const port = 3000;

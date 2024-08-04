f495c8c3c54a8357cc84d68bdfa912bbd06ea306
wilson alexander <wilgm0kdev@gmail.com>
2024-08-03 23:03:10 -0500

    otra forma de config las rutas

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,12 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
-router.get('/', function (req,res){
-    res.send('Hola mundo en express')
-})
-router.post('/', function (req,res){
-    res.json({msg:"respuesta de tipo post"})
-})
+// router.get('/', function (req,res){
+//     res.send('Hola mundo en express')
+// })
+// router.post('/', function (req,res){
+//     res.json({msg:"respuesta de tipo post"})
+// })
+
+router.route('/')
+    .get(function(req,res){
+        res.send('Hola mundo en express')
+    })
+    .post(function(req,res){
+        res.json({msg:"respuesta de tipo post"})
+    })
+
 
 
 export default router
